275547154ebc0787a907d25a79bcd0dfa3a4f93c
liujf <fddiljf@126.com>
2018-11-19 14:13:31 +0800

    d2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import './assets/iconfont/iconfont.css';
 import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<LocaleProvider locale={zhCN}><App /></LocaleProvider>,
     document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
--- src/pages/CallDll.jsx
+++ src/pages/CallDll.jsx
@@ -27,7 +27,7 @@ export default class Workbench extends Component {
                <Card style={{ marginTop: 16 }} loading={this.state.loading}>
                     <pre style={{ background: "#ececec", padding: 5 }}>
                          1、安装.netframwork4.5及以上版本。<br />
-                         2、安装vs编译打包工具 $npm install --global windows-build-tools<br />
+                         2、安装vs编译打包工具 $npm  --vs2015  install --global windows-build-tools --<br />
                          3、设置镜像地址方便快速下载[node 环境变量]set ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/<br />
                          4、安装ffi 使用$npm install https://github.com/node-ffi/node-ffi.git --save <br />
                          5、有些时候需要手工安装node-gyp  $npm install -g node-gyp<br />
