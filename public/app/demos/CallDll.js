00b7117108ed7d7e1b6aabdb4881e0cbdf4452ab
liujf <fddiljf@126.com>
2018-11-23 15:53:01 +0800

    d7

--- public/app/demos/CallDll.js
+++ public/app/demos/CallDll.js
@@ -1,14 +1,20 @@
 const { ipcMain } = require('electron')
 const path = require('path')
 const ffi = require('ffi-napi')
+const isDev = require('electron-is-dev')
 
 ipcMain.on('call-dll-test', (event, arg) => {
-    iopath = path.join(process.cwd(), '/addon/test-ai32.dll');
-    const {dialog} = require('electron')
-    dialog.showMessageBox({title:"路径",message:iopath,detail:iopath})
-    const testDll = ffi.Library(iopath, {
+    let ioPath = ""
+    if (isDev) {
+        ioPath = path.join(process.cwd(), '/addon/test-ai32.dll');
+    } else {
+        ioPath = path.join(process.cwd(), 'resources/addon/test-ai32.dll');
+    }
+    const { dialog } = require('electron')
+    dialog.showMessageBox({ title: "路径", message: ioPath, detail: ioPath })
+    const testDll = ffi.Library(ioPath, {
         'HelloWorld': ['void', []]
     })
     testDll.HelloWorld()
-    event.sender.send('call-dll-done', iopath)
+    event.sender.send('call-dll-done', ioPath)
 })
